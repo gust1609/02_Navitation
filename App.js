54bae9ca41de68a1254a45c61da4284435f88f3b
gust1609 <119044907+gust1609@users.noreply.github.com>
2024-09-18 15:46:54 +0200

    Add navigation and screens for Home, User Profile, and Settings

--- App.js
+++ App.js
@@ -1,5 +1,10 @@
 import { StatusBar } from 'expo-status-bar';
 import { StyleSheet, Text, View } from 'react-native';
+import HomeScreen from './HomeScreen';
+import UserProfileScreen from './UserProfileScreen';
+import SettingsScreen from './SettingsScreen';
+import { NavigationContainer } from '@react-navigation/native';
+import { createBottomTabNavigator } from '@react-navigation/bottom-tabs';
 
 export default function App() {
   return (
